e42a3fb09e29e22ba88f171ce9f87624a8b283de
Dhruv Patel <dhruvpatel2991998@gmail.com>
2026-05-08 15:29:08 +0200

    Update ros2-jazzy.rb to clone jazzy branch

--- Formula/ros2-jazzy.rb
+++ Formula/ros2-jazzy.rb
@@ -1,13 +1,15 @@
 class Ros2Jazzy < Formula
   desc "Pre-built ROS 2 Jazzy libraries for macOS"
   homepage "https://github.com/idesign0/homebrew-ros2"
-  # Point to your jazzy branch
-  url "https://github.com/idesign0/homebrew-ros2.git", :branch => "jazzy"
   version "0.1.0"
 
   def install
-    # This takes everything from your 'jazzy' branch and puts it in the Cellar
-    prefix.install Dir["*"]
+    system "git", "clone", "--branch", "jazzy",
+           "https://github.com/idesign0/homebrew-ros2.git", buildpath/"ros2"
+    cd buildpath/"ros2" do
+      system "git", "lfs", "pull"
+    end
+    prefix.install Dir[buildpath/"ros2/*"]
   end
 
   def caveats
