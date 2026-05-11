45d20397e9321e7a342d26a4361a63da6e86ebbf
Dhruv Patel <dhruvpatel2991998@gmail.com>
2026-05-11 16:36:51 +0200

    Modify ROS 2 Kilted formula for new version
    Updated formula for ROS 2 Kilted to new version and URL.

--- Formula/ros2-kilted.rb
+++ Formula/ros2-kilted.rb
@@ -1,19 +1,20 @@
 class Ros2Kilted < Formula
-  desc "Pre-built ROS 2 Kilted libraries for macOS"
+  desc "ROS 2 Kilted for macOS"
   homepage "https://github.com/idesign0/homebrew-ros2"
-  # Point to your kilted branch
-  url "https://github.com/idesign0/homebrew-ros2.git", :branch => "kilted"
-  version "0.1.0"
+
+  version "26.05.11.1"
+
+  url "https://github.com/idesign0/homebrew-ros2/releases/download/kilted-#{version}/ros2-kilted-macos-#{version}.tar.gz"
+
+  sha256 "REPLACE_SHA"
 
   def install
-    # This takes everything from your 'kilted' branch and puts it in the Cellar
-    prefix.install Dir["*"]
+    prefix.install Dir["install/*"]
   end
 
   def caveats
     <<~EOS
-      To use ROS 2 Kilted, you must source the setup script:
-        source #{opt_prefix}/setup.bash
+      source #{opt_prefix}/setup.bash
     EOS
   end
 end
