23b7ceb3eebe2ddd1a80892c8a1bd10bdd4df291
Dhruv Patel <dhruvpatel2991998@gmail.com>
2026-05-11 16:36:30 +0200

    Update ROS 2 Humble formula details
    Updated the description and version for ROS 2 Humble. Changed the installation path and updated the URL for the tarball.

--- Formula/ros2-humble.rb
+++ Formula/ros2-humble.rb
@@ -1,19 +1,20 @@
 class Ros2Humble < Formula
-  desc "Pre-built ROS 2 Humble libraries for macOS"
+  desc "ROS 2 Humble for macOS"
   homepage "https://github.com/idesign0/homebrew-ros2"
-  # Point to your humble branch
-  url "https://github.com/idesign0/homebrew-ros2.git", :branch => "humble"
-  version "0.1.0"
+
+  version "26.05.11.1"
+
+  url "https://github.com/idesign0/homebrew-ros2/releases/download/humble-#{version}/ros2-humble-macos-#{version}.tar.gz"
+
+  sha256 "REPLACE_SHA"
 
   def install
-    # This takes everything from your 'humble' branch and puts it in the Cellar
-    prefix.install Dir["*"]
+    prefix.install Dir["install/*"]
   end
 
   def caveats
     <<~EOS
-      To use ROS 2 humble, you must source the setup script:
-        source #{opt_prefix}/setup.bash
+      source #{opt_prefix}/setup.bash
     EOS
   end
 end
