d40bac3cf714da9828153db207a3072acabfc78a
Dhruv Patel <dhruvpatel2991998@gmail.com>
2026-05-11 16:35:29 +0200

    Update Ros2Jazzy formula with new version and URL

--- Formula/ros2-jazzy.rb
+++ Formula/ros2-jazzy.rb
@@ -1,19 +1,20 @@
 class Ros2Jazzy < Formula
-  desc "Pre-built ROS 2 Jazzy libraries for macOS"
+  desc "ROS 2 Jazzy for macOS"
   homepage "https://github.com/idesign0/homebrew-ros2"
-  # Point to your jazzy branch
-  url "https://github.com/idesign0/homebrew-ros2.git", :branch => "jazzy"
-  version "0.1.0"
+
+  version "26.05.11.1"
+
+  url "https://github.com/idesign0/homebrew-ros2/releases/download/jazzy-#{version}/ros2-jazzy-macos-#{version}.tar.gz"
+
+  sha256 "REPLACE_SHA"
 
   def install
-    # This takes everything from your 'jazzy' branch and puts it in the Cellar
-    prefix.install Dir["*"]
+    prefix.install Dir["install/*"]
   end
 
   def caveats
     <<~EOS
-      To use ROS 2 Jazzy, you must source the setup script:
-        source #{opt_prefix}/setup.bash
+      source #{opt_prefix}/setup.bash
     EOS
   end
 end
