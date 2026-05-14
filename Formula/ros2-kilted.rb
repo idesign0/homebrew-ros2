872249dc0838685575bd01e7b3b99efde8ce892a
Dhruv Patel <dhruvpatel2991998@gmail.com>
2026-05-14 11:44:01 +0200

    Add post_install method for jsoncpp symlink
    Add post-install step to create symlink for jsoncpp library compatibility.

--- Formula/ros2-kilted.rb
+++ Formula/ros2-kilted.rb
@@ -12,6 +12,16 @@ class Ros2Kilted < Formula
       prefix.install Dir["*"]
   end
 
+  def post_install
+    # jsoncpp ABI compat: build used .26, Homebrew ships .27+
+    jsoncpp_lib = Formula["jsoncpp"].opt_lib
+    symlink_26 = jsoncpp_lib/"libjsoncpp.26.dylib"
+    unless symlink_26.exist?
+      actual = jsoncpp_lib.glob("libjsoncpp.*.dylib").reject { |f| f.symlink? }.first
+      symlink_26.make_symlink(actual) if actual
+    end
+  end
+  
   def caveats
     <<~EOS
       source #{opt_prefix}/setup.bash
